e1a52fe1da4e23c3a4437d5ba2a58f11aadbde77
Anna <annakrygier@hotmail.com>
2016-11-26 13:30:26 -0800

    Configure webpack

---
+++ webpack.config.js
@@ -0,0 +1,73 @@
+const webpack = require('webpack');
+const indexer = require('html-webpack-plugin');
+const extractor = require('extract-text-webpack-plugin');
+
+module.exports = {
+	entry: {
+		app: __dirname + "/src/index.jsx",
+		style: __dirname + "/src/styles/style.css"
+	},
+	output: {
+		filename: "[name].bundle.js",
+		path: __dirname + "/dest",
+		chunkFilename: "bundle.js"
+	},
+	watch: true,
+	plugins: [
+		new indexer({
+			inject: 'body',
+			hash: false,
+			cache: true,
+			template: 'src/index.html',
+			showErrors: false
+		}),
+		new extractor('styles.css'),
+		new webpack.DefinePlugin({
+			'process.env': {
+				NODE_ENV: JSON.stringify('production')
+			}
+		}),
+		new webpack.optimize.UglifyJsPlugin({
+			beautify: false,
+			debug: true,
+			minimize: true,
+			sourceMap: false,
+			comments: false,
+			compress: {
+				warnings: false
+			},
+			mangle: {
+				except: ['$', 'webpackJsonp'],
+				screw_ie8: true,
+				keep_fnames: true
+			}
+		})],
+	module: {
+		loaders: [
+			{
+				test: /\.jsx?$/,
+				exclude: /node_modules/,
+				loader: 'react-hot'
+			},
+			{
+				test: /\.jsx?$/,
+				exclude: /node_modules/,
+				loader: 'babel-loader',
+				query: {
+					presets: ['es2015', 'react']
+				}
+			 },
+			{
+				test: /\.html$/,
+				loader: "html"
+			},
+			{
+				test: /\.css$/,
+				loader: extractor.extract("style-loader", "css-loader")
+			}
+		]
+	},
+	resolve: {
+		extensions: ['', '.js', '.jsx']
+	}
+}
